73d0f5ba6d23d0e4bf81b4cf3e2f8405b5a50a69
ubuntu tzh <2512243578@qq.com>
2017-03-27 16:13:32 +0800

    refresh the data!

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 from predict import *
+from deal_data import *
 import xlrd,xlwt
 import os
 import numpy as np
 
+x_name = '0'
+# max depth of drill!
+max_T = 102
+t_name = '0.txt'
 file_name = 'pred0'
 
-ptxt2xl(file_name)
 
+# ptxt2xl(file_name)
+
+txt2xl(t_name,x_name)
